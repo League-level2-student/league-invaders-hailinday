fc6a6c1abdf7c38ef0aa23579f9f40dbfc0be4d6
hailinday <54258211+hailinday@users.noreply.github.com>
2021-03-05 16:55:26 -0800

    League Invaders

---
+++ src/GamePanel.java
@@ -0,0 +1,50 @@
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+public class GamePanel extends JPanel {
+	final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+    int currentState = MENU;
+    Font titleFont;
+    GamePanel() {
+    	titleFont = new Font("Arial", Font.PLAIN, 48);
+    	titleFont1 = new Font("Arial", Font.PLAIN, 26);
+    	titleFont2 = new Font("Arial", Font.PLAIN, 26);
+    }
+	@Override
+	public void paintComponent(Graphics g){
+		if(currentState == MENU){
+		    drawMenuState(g);
+		}else if(currentState == GAME){
+		    drawGameState(g);
+		}else if(currentState == END){
+		    drawEndState(g);
+		}
+	}
+    void updateMenuState() {
+    	
+    }
+    void updateGameState() {
+    	
+    }
+    void updateEndState() {
+    	
+    }
+    void drawMenuState(Graphics g) {
+    	g.setColor(Color.BLUE);
+    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+    	g.setFont(titleFont);
+    	g.setColor(Color.YELLOW);
+    	g.drawString("League Invaders", 200, 200);
+    }
+    void drawGameState(Graphics g) {
+    	g.setColor(Color.BLACK);
+    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+    }
+    void drawEndState(Graphics g)  {
+    	g.setColor(Color.RED);
+    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+    }
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,21 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	JFrame frame = new JFrame();
+	GamePanel game = new GamePanel();
+	
+	
+	public static void main(String[] args) {
+		LeagueInvaders invader = new LeagueInvaders();
+		invader.setup();
+	}
+	
+	void setup() {
+		frame.add(game);
+		frame.setVisible(true);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
